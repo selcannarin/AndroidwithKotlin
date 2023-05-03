6290369bc489b067b855ab5a32c534aeccfca6a8
selcannarin <narinselcan@gmail.com>
2023-05-03 17:59:57 +0300

    Recycler Card View 1

--- MaterialDesign/CardRecyclerView/RecyclerCardView/app/src/main/java/com/selcannarin/recyclercardview1/RVAdapter.kt
+++ MaterialDesign/CardRecyclerView/RecyclerCardView/app/src/main/java/com/selcannarin/recyclercardview1/RVAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.PopupMenu
 import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
@@ -27,9 +28,31 @@ class RVAdapter(private val mContext:Context,private val countryList:List<Countr
     override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
         val country = countryList[position]
         holder.countryName.text = country.CountryName
+
         holder.lineCardView.setOnClickListener{
             Toast.makeText(mContext, "Country: ${country.CountryName}",Toast.LENGTH_SHORT).show()
         }
+        holder.imagePoint.setOnClickListener{
+
+            val popup = PopupMenu(mContext,holder.imagePoint)
+            popup.menuInflater.inflate(R.menu.popup_menu,popup.menu)
+            popup.show()
+
+            popup.setOnMenuItemClickListener { item->
+                when(item.itemId){
+                    R.id.Item_delete -> {
+                        Toast.makeText(mContext,"Delete: ${country.CountryName}",Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.Item_save -> {
+                        Toast.makeText(mContext,"Save: ${country.CountryName}",Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
+
     }
 
     override fun getItemCount(): Int {
