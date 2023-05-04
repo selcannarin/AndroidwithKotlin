11329bde305fc441bf222917a13694f1850f12da
selcannarin <narinselcan@gmail.com>
2023-05-04 23:57:28 +0300

    Tab Layout

--- MaterialDesign/TabLayout/app/src/main/java/com/selcannarin/tablayout/MainActivity.kt
+++ MaterialDesign/TabLayout/app/src/main/java/com/selcannarin/tablayout/MainActivity.kt
@@ -2,14 +2,47 @@ package com.selcannarin.tablayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import com.selcannarin.tablayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private val fragmentList = ArrayList<Fragment>()
+    private val fragmentTitleList = ArrayList<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        fragmentList.add(FirstFragment())
+        fragmentList.add(SecondFragment())
+        fragmentList.add(ThirdFragment())
+
+        val adapter = ViewPageAdapter(this)
+        binding.viewPager2.adapter = adapter
+
+        fragmentTitleList.add("1")
+        fragmentTitleList.add("2")
+        fragmentTitleList.add("3")
+
+        TabLayoutMediator(binding.tabLayout,binding.viewPager2){tab, position->
+            tab.setText(fragmentTitleList[position])
+        }.attach()
+
+    }
+    inner class ViewPageAdapter (fragmentActivity: FragmentActivity):FragmentStateAdapter(fragmentActivity){
+        override fun getItemCount(): Int {
+            return fragmentList.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return fragmentList[position]
+        }
+
     }
 }
